bcd75b758eba65f3d9b8c208e748fdc2cc824c91
anshitakadam <130995617+anshitakadam@users.noreply.github.com>
2023-12-18 19:54:34 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import os
-import re
-import json
 import streamlit as st
+import json
+import os
 
-# To help construct our Chat Messages
 from langchain.schema import HumanMessage
 from langchain.prompts import PromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
 
@@ -13,7 +11,7 @@ from langchain.chat_models import ChatOpenAI
 # To parse outputs and get structured data back
 from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 
-# use your OpenAI API secret key
+# use OpenAI API secret key
 os.environ["OPENAI_API_KEY"] = "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"
 
  # The schema I want out
@@ -26,15 +24,14 @@ response_schemas = [
 # The parser that will look for the LLM output in my schema and return it back to me
 output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
 
-# The format instructions that LangChain makes. Let's look at them
+# The format instructions that LangChain makes
 format_instructions = output_parser.get_format_instructions()
 
 print(format_instructions)
 
-# create ChatGPT object
 chat_model = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
 
-# The prompt template that brings it all together
+# The prompt template that brings it together
 prompt = ChatPromptTemplate(
     messages=[
         HumanMessagePromptTemplate.from_template("""Given a text input from the user, generate multiple choice questions 
@@ -44,77 +41,101 @@ prompt = ChatPromptTemplate(
     input_variables=["user_prompt"],
     partial_variables={"format_instructions": format_instructions}
 )
-
-st.title("AnshitaAI Quiz Generator")
-user_prompt = st.text_input("Enter a quiz topic.")
-button = st.button("Generate")
-score=0
-if user_prompt and button:
- with st.spinner("Generating..."):
-
-    
-
-  text = 'Generate a general knowledge quiz with 3 multiple choice questions with 4 options each. The options should be labelled 1, 2, 3, 4. The topic is ' + user_prompt 
-  user_query = prompt.format_prompt(user_prompt = text)
-
-  user_query_output = chat_model(user_query.to_messages())
-
-# Given plain string containing multiple queries
-  queries_string = user_query_output.content
-  print(queries_string)
-
-
-# Split the string into individual queries
-
-# List to store information for each question
-  questions_info = []
-
-  json_strings = []
-  start_pos = 0
-
-  while True:
-    start_pos = queries_string.find('{', start_pos)
-    if start_pos == -1:
-        break
-    end_pos = queries_string.find('}', start_pos) + 1
-    json_strings.append(queries_string[start_pos:end_pos])
-    start_pos = end_pos
-
-# Convert each substring to a JSON object
-  json_objects = [json.loads(json_str) for json_str in json_strings]
-
-# Print the resulting JSON objects
-  print(json.dumps(json_objects, indent=2))
-
-  quiz_data = []
-
-  for obj in json_objects:
-    question_data = {
-        "question": obj.get("question", ""),
-        "options": [option.strip() for option in obj.get("options", "").split('\n')],
-        "answer": obj.get("answer", "")
-    }
-    quiz_data.append(question_data)
-
-  for question_data in quiz_data:
-    print("Question:", question_data["question"])
-    print("Options:")
-    for i, option in enumerate(question_data["options"], start=1):
-        print(f"{i}. {option}")
-    print("Correct Answer:", question_data["answer"])
-    print()
-    
-  for i in range(2):
-    st.write(quiz_data[i]['question'])  #this prints the question
-    q=st.radio(quiz_data[i]['question'],[
-      quiz_data[i]['options'][0],quiz_data[i]['options'][1],quiz_data[i]['options'][2],quiz_data[i]['options'][3]],
-      key=i,index=None)
-    if q==quiz_data[i]['answer']:
-      score = score+1
-      st.write('Correct') 
-    else:
-     st.write('Incorrect, correct answer was '+ quiz_data[i]['answer'])
-     
-st.write(score) 
-    
-
+ 
+# Function to initialize session state
+def initialize_session():
+    return st.session_state.get("score", 0), st.session_state.get("current_question", 0), st.session_state.get("selected_option", None)
+ 
+# Sample quiz data
+quiz_data = []
+ 
+# Function to load quiz data from JSON strings
+def load_quiz_data(queries_string):
+    global quiz_data
+    json_strings = []
+    start_pos = 0
+ 
+    while True:
+        start_pos = queries_string.find('{', start_pos)
+        if start_pos == -1:
+            break
+        end_pos = queries_string.find('}', start_pos) + 1
+        json_strings.append(queries_string[start_pos:end_pos])
+        start_pos = end_pos
+ 
+    # Convert each substring to a JSON object
+    json_objects = [json.loads(json_str) for json_str in json_strings]
+ 
+    # Print the resulting JSON objects
+    print(json.dumps(json_objects, indent=2))
+ 
+    for obj in json_objects:
+        question_data = {
+            "question": obj.get("question", ""),
+            "options": [option.strip() for option in obj.get("options", "").split('\n')],
+            "answer": obj.get("answer", "")
+        }
+        quiz_data.append(question_data)
+ 
+# Streamlit App
+def main():
+    st.title("AnshitaAI Quiz Generator")
+ 
+    # Initialize session state
+    score, current_question, selected_option = initialize_session()
+ 
+    # Quiz Generation
+    user_prompt = st.text_input("Enter a quiz topic.")
+    button = st.button("Generate")
+ 
+    if user_prompt and button:
+        with st.spinner("Generating..."):
+ 
+            # Construct prompt for quiz generation
+            text = 'Generate a general knowledge quiz with 3 multiple choice questions with 4 options each. The options should be labelled 1, 2, 3, 4. The topic is ' + user_prompt 
+            user_query = prompt.format_prompt(user_prompt = text)
+            user_query_output = chat_model(user_query.to_messages())
+ 
+            # Parse outputs and load quiz data
+            queries_string = user_query_output.content
+            load_quiz_data(queries_string)
+ 
+    # Quiz App
+    if quiz_data:
+        # Check if the user has made a selection
+        if selected_option is not None:
+            # Check if the selected option is correct
+            current_question_data = quiz_data[current_question]
+            if selected_option == current_question_data['answer']:
+                st.success("Correct!")
+                score += 1
+            else:
+                st.error(f"Wrong! Correct answer: {current_question_data['answer']}")
+ 
+        # Display the current question
+        current_question_data = quiz_data[current_question]
+        st.header(f"Question {current_question + 1}")
+        st.markdown(f"**{current_question_data['question']}**")
+ 
+        # Display radio buttons for options
+        selected_option = st.radio(f"Options:", current_question_data['options'])
+ 
+        st.write("---")  # Separator between questions
+ 
+        # Display final score when all questions are answered
+        if current_question == len(quiz_data) - 1:
+            st.title("Quiz Completed")
+            st.success(f"Your Final Score: {score}/{len(quiz_data)}")
+        else:
+            # Button to move to the next question
+            if st.button("Next Question"):
+                current_question += 1
+                selected_option = None  # Reset selected option for the new question
+ 
+        # Update session state
+        st.session_state.score = score
+        st.session_state.current_question = current_question
+        st.session_state.selected_option = selected_option
+ 
+if __name__ == "__main__":
+    main()
